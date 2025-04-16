openHiTLS/openHiTLS

feat: tls kem support hybrid kex: X25519MLKEM768 SecP256r1MLKEM768 SecP384r1MLKEM1024

tls/crypt/crypt_adapt/crypt.c
@@ -91,6 +91,8 @@ const char *g_cryptCallBackStr[] = {
 
     [HITLS_CRYPT_CALLBACK_HKDF_EXTRACT] = "HKDF-Extract",
     [HITLS_CRYPT_CALLBACK_HKDF_EXPAND] = "HKDF-Expand",
+    [HITLS_CRYPT_CALLBACK_KEM_ENCAPSULATE] = "KEM-Encapsulate",
+    [HITLS_CRYPT_CALLBACK_KEM_DECAPSULATE] = "KEM-Decapsulate",
 };
 
 #ifndef HITLS_TLS_FEATURE_PROVIDER
@@ -827,18 +829,60 @@ uint32_t SAL_CRYPT_GetCryptLength(const TLS_Ctx *ctx, int32_t cmd, int32_t param
     if (ctx == NULL) {
         return 0;
     }
+    groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, (uint16_t)param);
     switch (cmd) {
         case HITLS_CRYPT_INFO_CMD_GET_PUBLIC_KEY_LEN:
-            groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, (uint16_t)param);
             if (groupInfo == NULL) {
                 return 0;
             }
             return groupInfo->pubkeyLen;
+        case HITLS_CRYPT_INFO_CMD_GET_CIPHERTEXT_LEN:
+            if (groupInfo == NULL) {
+                return 0;
+            }
+            return groupInfo->ciphertextLen;
         default:
             return 0;
     }
     return 0;
 }
+#ifdef HITLS_TLS_FEATURE_KEM
+int32_t SAL_CRYPT_KemEncapsulate(TLS_Ctx *ctx, HITLS_KemEncapsulateParams *params)
+{
+#ifdef HITLS_TLS_FEATURE_PROVIDER
+    int32_t ret = HITLS_CRYPT_KemEncapsulate(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
+        &ctx->config.tlsConfig, params);
+#else
+    (void)ctx;
+    (void)params;
+    if (g_cryptEcdhMethod.kemEncapsulate == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16617, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "kemEncapsulate callback not registered", 0, 0, 0, 0);
+        return HITLS_UNREGISTERED_CALLBACK;
+    }
 
+    int32_t ret = g_cryptEcdhMethod.kemEncapsulate(params);
+#endif
+    return CheckCallBackRetVal(HITLS_CRYPT_CALLBACK_KEM_ENCAPSULATE, ret, BINLOG_ID16617,
+        HITLS_CRYPT_ERR_KEM_ENCAPSULATE);
+}
 
+int32_t SAL_CRYPT_KemDecapsulate(HITLS_CRYPT_Key *key, const uint8_t *ciphertext, uint32_t ciphertextLen,
+    uint8_t *sharedSecret, uint32_t *sharedSecretLen)
+{
+#ifdef HITLS_TLS_FEATURE_PROVIDER
+    int32_t ret = HITLS_CRYPT_KemDecapsulate(key, ciphertext, ciphertextLen, sharedSecret, sharedSecretLen);
+#else
+    if (g_cryptEcdhMethod.kemDecapsulate == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16617, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "kemDecapsulate callback not registered", 0, 0, 0, 0);
+        return HITLS_UNREGISTERED_CALLBACK;
+    }
+
+    int32_t ret = g_cryptEcdhMethod.kemDecapsulate(key, ciphertext, ciphertextLen, sharedSecret, sharedSecretLen);
+#endif
+    return CheckCallBackRetVal(HITLS_CRYPT_CALLBACK_KEM_DECAPSULATE, ret, BINLOG_ID16617,
+        HITLS_CRYPT_ERR_KEM_DECAPSULATE);
+}
+#endif /* HITLS_TLS_FEATURE_KEM */
 #endif /* HITLS_TLS_PROTO_TLS13 */

tls/handshake/common/src/tls13key.c
@@ -27,6 +27,7 @@
 #include "hs_kx.h"
 #include "hs_common.h"
 #include "transcript_hash.h"
+#include "config_type.h"
 
 int32_t HS_TLS13DeriveSecret(CRYPT_KeyDeriveParameters *deriveInfo, bool isHashed, uint8_t *outSecret, uint32_t outLen)
 {
@@ -177,6 +178,52 @@ int32_t HS_TLS13DeriveNextStageSecret(HITLS_Lib_Ctx *libCtx, const char *attrNam
     return TLS13HkdfExtract(libCtx, attrName, &extractInput, outSecret, outLen);
 }
 
+int32_t TLS13DeriveDheSecret(TLS_Ctx *ctx, uint8_t *preMasterSecret, uint32_t *preMasterSecretLen, uint32_t hashLen)
+{
+    KeyExchCtx *keyCtx = ctx->hsCtx->kxCtx;
+    if (keyCtx->peerPubkey == NULL) {
+        *preMasterSecretLen = hashLen;
+        return HITLS_SUCCESS;
+    }
+
+    const TLS_GroupInfo *groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, ctx->negotiatedInfo.negotiatedGroup);
+    if (groupInfo == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16895, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "group info not found", 0, 0, 0, 0);
+        return HITLS_INVALID_INPUT;
+    }
+    if (!groupInfo->isKem) {
+        return SAL_CRYPT_CalcEcdhSharedSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
+                keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
+                preMasterSecret, preMasterSecretLen);
+    }
+#ifdef HITLS_TLS_FEATURE_KEM
+    if (ctx->isClient) {
+        return SAL_CRYPT_KemDecapsulate(keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
+            preMasterSecret, preMasterSecretLen);
+    }
+    BSL_SAL_Free(keyCtx->ciphertext);
+    keyCtx->ciphertext = BSL_SAL_Calloc(1, groupInfo->ciphertextLen);
+    if (keyCtx->ciphertext == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16895, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "ciphertext malloc fail", 0, 0, 0, 0);
+        return HITLS_MEMALLOC_FAIL;
+    }
+    keyCtx->ciphertextLen = groupInfo->ciphertextLen;
+    return SAL_CRYPT_KemEncapsulate(ctx,
+        &(HITLS_KemEncapsulateParams){
+            .groupId = ctx->negotiatedInfo.negotiatedGroup,
+            .peerPubkey = keyCtx->peerPubkey,
+            .pubKeyLen = keyCtx->pubKeyLen,
+            .ciphertext = keyCtx->ciphertext,
+            .ciphertextLen = &keyCtx->ciphertextLen,
+            .sharedSecret = preMasterSecret,
+            .sharedSecretLen = preMasterSecretLen,
+        });
+#else
+    return HITLS_INTERNAL_EXCEPTION;
+#endif
+}
 /*
         Early Secret
              |
@@ -196,20 +243,11 @@ int32_t TLS13DeriveHandshakeSecret(TLS_Ctx *ctx)
     }
     uint8_t preMasterSecret[MAX_PRE_MASTER_SECRET_SIZE] = {0};
     uint32_t preMasterSecretLen = MAX_PRE_MASTER_SECRET_SIZE;
-    KeyExchCtx *keyCtx = ctx->hsCtx->kxCtx;
-    int32_t ret;
-    if (keyCtx->peerPubkey != NULL) {
-        ret = SAL_CRYPT_CalcEcdhSharedSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
-            keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
-            preMasterSecret, &preMasterSecretLen);
-        if (ret != HITLS_SUCCESS) {
-            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16894, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
-                "CalcEcdhSharedSecret fail", 0, 0, 0, 0);
-            return ret;
-        }
-    } else {
-        /* In psk-only mode, the key is an all-zero array of the hash length. */
-        preMasterSecretLen = hashLen;
+    int32_t ret = TLS13DeriveDheSecret(ctx, preMasterSecret, &preMasterSecretLen, hashLen);
+    if (ret != HITLS_SUCCESS) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16894, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "DeriveDheSecret fail", 0, 0, 0, 0);
+        return ret;
     }
     uint32_t handshakeSecretLen = hashLen;
     ret = HS_TLS13DeriveNextStageSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),

tls/handshake/send/src/send_server_hello.c
@@ -32,6 +32,8 @@
 #include "pack.h"
 #include "send_process.h"
 #include "hs_kx.h"
+#include "config_type.h"
+
 #if defined(HITLS_TLS_PROTO_TLS_BASIC) || defined(HITLS_TLS_PROTO_DTLS12)
 #ifdef HITLS_TLS_FEATURE_SESSION
 static int32_t ServerPrepareSessionId(TLS_Ctx *ctx)
@@ -207,7 +209,15 @@ static int32_t Tls13ServerPrepareKeyShare(TLS_Ctx *ctx)
                                    need to be calculated again */
         return HITLS_SUCCESS;
     }
-
+    const TLS_GroupInfo *groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, keyShare->group);
+    if (groupInfo == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16895, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "group info not found", 0, 0, 0, 0);
+        return HITLS_INVALID_INPUT;
+    }
+    if (groupInfo->isKem) {
+        return HITLS_SUCCESS;
+    }
     HITLS_ECParameters curveParams = {
         .type = HITLS_EC_CURVE_TYPE_NAMED_CURVE,
         .param.namedcurve = keyShare->group,
@@ -254,18 +264,19 @@ int32_t Tls13ServerSendServerHelloProcess(TLS_Ctx *ctx)
             return ret;
         }
 
-        ret = HS_PackMsg(ctx, SERVER_HELLO, hsCtx->msgBuf, hsCtx->bufferLen, &hsCtx->msgLen);
-        if (ret != HITLS_SUCCESS) {
-            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID15555, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
-                "pack tls1.3 server hello msg fail.", 0, 0, 0, 0);
-            return ret;
-        }
         /* Server secret derivation */
         ret = HS_TLS13CalcServerHelloProcessSecret(ctx);
         if (ret != HITLS_SUCCESS) {
             BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16190, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
                 "Derive-Sevret failed.", 0, 0, 0, 0);
             ctx->method.sendAlert(ctx, ALERT_LEVEL_FATAL, ALERT_ILLEGAL_PARAMETER);
+            return ret;
+        }
+
+        ret = HS_PackMsg(ctx, SERVER_HELLO, hsCtx->msgBuf, hsCtx->bufferLen, &hsCtx->msgLen);
+        if (ret != HITLS_SUCCESS) {
+            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID15555, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+                "pack tls1.3 server hello msg fail.", 0, 0, 0, 0);
             return ret;
         }
     }

openharmony/third_party_openhitls

Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/198

tls/crypt/crypt_adapt/crypt.c
@@ -91,6 +91,8 @@ const char *g_cryptCallBackStr[] = {
 
     [HITLS_CRYPT_CALLBACK_HKDF_EXTRACT] = "HKDF-Extract",
     [HITLS_CRYPT_CALLBACK_HKDF_EXPAND] = "HKDF-Expand",
+    [HITLS_CRYPT_CALLBACK_KEM_ENCAPSULATE] = "KEM-Encapsulate",
+    [HITLS_CRYPT_CALLBACK_KEM_DECAPSULATE] = "KEM-Decapsulate",
 };
 
 #ifndef HITLS_TLS_FEATURE_PROVIDER
@@ -807,7 +809,7 @@ int32_t SAL_CRYPT_HkdfExpandLabel(CRYPT_KeyDeriveParameters *deriveInfo, uint8_t
     info.ctx = deriveInfo->seed;
     int32_t ret = SAL_CRYPT_EncodeHkdfLabel(&info, hkdfLabel, TLS13_MAX_HKDF_LABEL_LEN, &hkdfLabelLen);
     if (ret != HITLS_SUCCESS) {
-        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16617, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16626, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
             "EncodeHkdfLabel fail", 0, 0, 0, 0);
         return ret;
     }
@@ -827,18 +829,60 @@ uint32_t SAL_CRYPT_GetCryptLength(const TLS_Ctx *ctx, int32_t cmd, int32_t param
     if (ctx == NULL) {
         return 0;
     }
+    groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, (uint16_t)param);
     switch (cmd) {
         case HITLS_CRYPT_INFO_CMD_GET_PUBLIC_KEY_LEN:
-            groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, (uint16_t)param);
             if (groupInfo == NULL) {
                 return 0;
             }
             return groupInfo->pubkeyLen;
+        case HITLS_CRYPT_INFO_CMD_GET_CIPHERTEXT_LEN:
+            if (groupInfo == NULL) {
+                return 0;
+            }
+            return groupInfo->ciphertextLen;
         default:
             return 0;
     }
     return 0;
 }
+#ifdef HITLS_TLS_FEATURE_KEM
+int32_t SAL_CRYPT_KemEncapsulate(TLS_Ctx *ctx, HITLS_KemEncapsulateParams *params)
+{
+#ifdef HITLS_TLS_FEATURE_PROVIDER
+    int32_t ret = HITLS_CRYPT_KemEncapsulate(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
+        &ctx->config.tlsConfig, params);
+#else
+    (void)ctx;
+    (void)params;
+    if (g_cryptEcdhMethod.kemEncapsulate == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16627, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "kemEncapsulate callback not registered", 0, 0, 0, 0);
+        return HITLS_UNREGISTERED_CALLBACK;
+    }
 
+    int32_t ret = g_cryptEcdhMethod.kemEncapsulate(params);
+#endif
+    return CheckCallBackRetVal(HITLS_CRYPT_CALLBACK_KEM_ENCAPSULATE, ret, BINLOG_ID16617,
+        HITLS_CRYPT_ERR_KEM_ENCAPSULATE);
+}
 
+int32_t SAL_CRYPT_KemDecapsulate(HITLS_CRYPT_Key *key, const uint8_t *ciphertext, uint32_t ciphertextLen,
+    uint8_t *sharedSecret, uint32_t *sharedSecretLen)
+{
+#ifdef HITLS_TLS_FEATURE_PROVIDER
+    int32_t ret = HITLS_CRYPT_KemDecapsulate(key, ciphertext, ciphertextLen, sharedSecret, sharedSecretLen);
+#else
+    if (g_cryptEcdhMethod.kemDecapsulate == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16630, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "kemDecapsulate callback not registered", 0, 0, 0, 0);
+        return HITLS_UNREGISTERED_CALLBACK;
+    }
+
+    int32_t ret = g_cryptEcdhMethod.kemDecapsulate(key, ciphertext, ciphertextLen, sharedSecret, sharedSecretLen);
+#endif
+    return CheckCallBackRetVal(HITLS_CRYPT_CALLBACK_KEM_DECAPSULATE, ret, BINLOG_ID16637,
+        HITLS_CRYPT_ERR_KEM_DECAPSULATE);
+}
+#endif /* HITLS_TLS_FEATURE_KEM */
 #endif /* HITLS_TLS_PROTO_TLS13 */

tls/handshake/common/src/tls13key.c
@@ -27,6 +27,7 @@
 #include "hs_kx.h"
 #include "hs_common.h"
 #include "transcript_hash.h"
+#include "config_type.h"
 
 int32_t HS_TLS13DeriveSecret(CRYPT_KeyDeriveParameters *deriveInfo, bool isHashed, uint8_t *outSecret, uint32_t outLen)
 {
@@ -177,6 +178,52 @@ int32_t HS_TLS13DeriveNextStageSecret(HITLS_Lib_Ctx *libCtx, const char *attrNam
     return TLS13HkdfExtract(libCtx, attrName, &extractInput, outSecret, outLen);
 }
 
+int32_t TLS13DeriveDheSecret(TLS_Ctx *ctx, uint8_t *preMasterSecret, uint32_t *preMasterSecretLen, uint32_t hashLen)
+{
+    KeyExchCtx *keyCtx = ctx->hsCtx->kxCtx;
+    if (keyCtx->peerPubkey == NULL) {
+        *preMasterSecretLen = hashLen;
+        return HITLS_SUCCESS;
+    }
+
+    const TLS_GroupInfo *groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, ctx->negotiatedInfo.negotiatedGroup);
+    if (groupInfo == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16244, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "group info not found", 0, 0, 0, 0);
+        return HITLS_INVALID_INPUT;
+    }
+    if (!groupInfo->isKem) {
+        return SAL_CRYPT_CalcEcdhSharedSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
+                keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
+                preMasterSecret, preMasterSecretLen);
+    }
+#ifdef HITLS_TLS_FEATURE_KEM
+    if (ctx->isClient) {
+        return SAL_CRYPT_KemDecapsulate(keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
+            preMasterSecret, preMasterSecretLen);
+    }
+    BSL_SAL_Free(keyCtx->ciphertext);
+    keyCtx->ciphertext = BSL_SAL_Calloc(1, groupInfo->ciphertextLen);
+    if (keyCtx->ciphertext == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16245, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "ciphertext malloc fail", 0, 0, 0, 0);
+        return HITLS_MEMALLOC_FAIL;
+    }
+    keyCtx->ciphertextLen = groupInfo->ciphertextLen;
+    return SAL_CRYPT_KemEncapsulate(ctx,
+        &(HITLS_KemEncapsulateParams){
+            .groupId = ctx->negotiatedInfo.negotiatedGroup,
+            .peerPubkey = keyCtx->peerPubkey,
+            .pubKeyLen = keyCtx->pubKeyLen,
+            .ciphertext = keyCtx->ciphertext,
+            .ciphertextLen = &keyCtx->ciphertextLen,
+            .sharedSecret = preMasterSecret,
+            .sharedSecretLen = preMasterSecretLen,
+        });
+#else
+    return HITLS_INTERNAL_EXCEPTION;
+#endif
+}
 /*
         Early Secret
              |
@@ -196,20 +243,11 @@ int32_t TLS13DeriveHandshakeSecret(TLS_Ctx *ctx)
     }
     uint8_t preMasterSecret[MAX_PRE_MASTER_SECRET_SIZE] = {0};
     uint32_t preMasterSecretLen = MAX_PRE_MASTER_SECRET_SIZE;
-    KeyExchCtx *keyCtx = ctx->hsCtx->kxCtx;
-    int32_t ret;
-    if (keyCtx->peerPubkey != NULL) {
-        ret = SAL_CRYPT_CalcEcdhSharedSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),
-            keyCtx->key, keyCtx->peerPubkey, keyCtx->pubKeyLen,
-            preMasterSecret, &preMasterSecretLen);
-        if (ret != HITLS_SUCCESS) {
-            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16894, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
-                "CalcEcdhSharedSecret fail", 0, 0, 0, 0);
-            return ret;
-        }
-    } else {
-        /* In psk-only mode, the key is an all-zero array of the hash length. */
-        preMasterSecretLen = hashLen;
+    int32_t ret = TLS13DeriveDheSecret(ctx, preMasterSecret, &preMasterSecretLen, hashLen);
+    if (ret != HITLS_SUCCESS) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16894, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "DeriveDheSecret fail", 0, 0, 0, 0);
+        return ret;
     }
     uint32_t handshakeSecretLen = hashLen;
     ret = HS_TLS13DeriveNextStageSecret(LIBCTX_FROM_CTX(ctx), ATTRIBUTE_FROM_CTX(ctx),

tls/handshake/send/src/send_server_hello.c
@@ -32,6 +32,8 @@
 #include "pack.h"
 #include "send_process.h"
 #include "hs_kx.h"
+#include "config_type.h"
+
 #if defined(HITLS_TLS_PROTO_TLS_BASIC) || defined(HITLS_TLS_PROTO_DTLS12)
 #ifdef HITLS_TLS_FEATURE_SESSION
 static int32_t ServerPrepareSessionId(TLS_Ctx *ctx)
@@ -163,7 +165,7 @@ int32_t ServerSendServerHelloProcess(TLS_Ctx *ctx)
             ret = DowngradeServerRandom(ctx);
             if (ret != EOK) {
                 BSL_ERR_PUSH_ERROR(HITLS_MEMCPY_FAIL);
-                BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16135, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+                BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16248, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
                     "copy down grade random fail.", 0, 0, 0, 0);
                 return HITLS_MEMCPY_FAIL;
             }
@@ -207,7 +209,15 @@ static int32_t Tls13ServerPrepareKeyShare(TLS_Ctx *ctx)
                                    need to be calculated again */
         return HITLS_SUCCESS;
     }
-
+    const TLS_GroupInfo *groupInfo = ConfigGetGroupInfo(&ctx->config.tlsConfig, keyShare->group);
+    if (groupInfo == NULL) {
+        BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16243, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+            "group info not found", 0, 0, 0, 0);
+        return HITLS_INVALID_INPUT;
+    }
+    if (groupInfo->isKem) {
+        return HITLS_SUCCESS;
+    }
     HITLS_ECParameters curveParams = {
         .type = HITLS_EC_CURVE_TYPE_NAMED_CURVE,
         .param.namedcurve = keyShare->group,
@@ -254,18 +264,19 @@ int32_t Tls13ServerSendServerHelloProcess(TLS_Ctx *ctx)
             return ret;
         }
 
-        ret = HS_PackMsg(ctx, SERVER_HELLO, hsCtx->msgBuf, hsCtx->bufferLen, &hsCtx->msgLen);
-        if (ret != HITLS_SUCCESS) {
-            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID15555, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
-                "pack tls1.3 server hello msg fail.", 0, 0, 0, 0);
-            return ret;
-        }
         /* Server secret derivation */
         ret = HS_TLS13CalcServerHelloProcessSecret(ctx);
         if (ret != HITLS_SUCCESS) {
             BSL_LOG_BINLOG_FIXLEN(BINLOG_ID16190, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
                 "Derive-Sevret failed.", 0, 0, 0, 0);
             ctx->method.sendAlert(ctx, ALERT_LEVEL_FATAL, ALERT_ILLEGAL_PARAMETER);
+            return ret;
+        }
+
+        ret = HS_PackMsg(ctx, SERVER_HELLO, hsCtx->msgBuf, hsCtx->bufferLen, &hsCtx->msgLen);
+        if (ret != HITLS_SUCCESS) {
+            BSL_LOG_BINLOG_FIXLEN(BINLOG_ID15555, BSL_LOG_LEVEL_ERR, BSL_LOG_BINLOG_TYPE_RUN,
+                "pack tls1.3 server hello msg fail.", 0, 0, 0, 0);
             return ret;
         }
     }
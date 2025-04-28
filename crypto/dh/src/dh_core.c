openHiTLS/openHiTLS

DH algorithm performance optimization.

crypto/dh/src/dh_core.c
@@ -471,14 +471,7 @@ int32_t CRYPT_DH_GetPara(const CRYPT_DH_Ctx *ctx, BSL_Param *param)
     return GetDhParamG(ctx->para, param);
 }
 
-/**
-    y != 0
-    y != 1
-    y < p - 1
-    (y ^ q) mod p == 1
-*/
-static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP,
-    const BN_BigNum *q, BN_Mont *montP, BN_Optimizer *opt)
+static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP)
 {
     // y != 0, y != 1
     if (BN_IsZero(y) || BN_IsOne(y)) {
@@ -490,27 +483,6 @@ static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP,
         BSL_ERR_PUSH_ERROR(CRYPT_DH_KEYINFO_ERROR);
         return CRYPT_DH_KEYINFO_ERROR;
     }
-    if (q == NULL) {
-        return CRYPT_SUCCESS; // The parameter q does not exist, this function is ended early.
-    }
-    // Verify q.
-    BN_BigNum *r = BN_Create(BN_Bits(minP));
-    if (r == NULL) {
-        BSL_ERR_PUSH_ERROR(CRYPT_MEM_ALLOC_FAIL);
-        return CRYPT_MEM_ALLOC_FAIL;
-    }
-    int32_t ret = BN_MontExp(r, y, q, montP, opt);
-    if (ret != CRYPT_SUCCESS) {
-        BN_Destroy(r);
-        BSL_ERR_PUSH_ERROR(ret);
-        return ret;
-    }
-    if (!BN_IsOne(r)) {
-        BN_Destroy(r);
-        BSL_ERR_PUSH_ERROR(CRYPT_DH_KEYINFO_ERROR);
-        return CRYPT_DH_KEYINFO_ERROR;
-    }
-    BN_Destroy(r);
     return CRYPT_SUCCESS;
 }
 
@@ -538,18 +510,70 @@ static void RefreshCtx(CRYPT_DH_Ctx *dhCtx, BN_BigNum *x, BN_BigNum *y, int32_t 
     }
 }
 
-int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx)
+/* SP800-56Ar3 5.6.1.1.4 Key-Pair Generation by Testing Candidates */
+static int32_t DH_GenSp80056ATestCandidates(CRYPT_DH_Ctx *ctx)
 {
-    if (ctx == NULL) {
-        BSL_ERR_PUSH_ERROR(CRYPT_NULL_INPUT);
-        return CRYPT_NULL_INPUT;
-    }
-    if (ctx->para == NULL) {
+    int32_t ret;
+    uint32_t bits = BN_Bits(ctx->para->p);
+    uint32_t qbits = BN_Bits(ctx->para->q);
+    /* If s is not the maximum security strength that can be support by (p, q, g), then return an error. */
+    int32_t s = CRYPT_DH_GetSecBits(ctx);
+    if (bits == 0 || qbits == 0 || s == 0) {
         BSL_ERR_PUSH_ERROR(CRYPT_DH_PARA_ERROR);
         return CRYPT_DH_PARA_ERROR;
     }
+    /* 2*s <= n <= len(q), set n = 2*s */
+    uint32_t n = 2 * s;
+    BN_BigNum *x = BN_Create(bits);
+    BN_BigNum *y = BN_Create(bits);
+    BN_BigNum *twoPowN = BN_Create(n);
+    BN_Mont *mont = BN_MontCreate(ctx->para->p);
+    BN_BigNum *m = ctx->para->q;
+    BN_Optimizer *opt = BN_OptimizerCreate();
+    if (x == NULL || y == NULL || twoPowN == NULL || mont == NULL || m == NULL) {
+        ret = CRYPT_MEM_ALLOC_FAIL;
+        BSL_ERR_PUSH_ERROR(CRYPT_MEM_ALLOC_FAIL);
+        goto ERR;
+    }
+    ret = BN_SetLimb(twoPowN, 1);
+    if (ret != CRYPT_SUCCESS) {
+        BSL_ERR_PUSH_ERROR(ret);
+        goto ERR;
+    }
+    ret = BN_Lshift(twoPowN, twoPowN, n);
+    if (ret != CRYPT_SUCCESS) {
+        BSL_ERR_PUSH_ERROR(ret);
+        goto ERR;
+    }
+    /* Set M = min(2^N, q), the minimum of 2^N and q. */
+    if (BN_Cmp(twoPowN, m) < 0) {
+        m = twoPowN;
+    }
+    for (int32_t cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
+        /* c in the interval [0, 2N - 1] */
+        if (BN_Cmp(x, m) >= 0) {
+            continue;
+        }
+        ret = BN+MontExpConsttime(y, ctx->para->g, x, mont, opt);
+        if (ret != CRYPT_SUCCESS) {
+            BSL_ERR_PUSH_ERROR(ret);
+        }
+        goto ERR; // The function exits successfully.
+    }
+    ret = CRYPT_DH_RAND_GENERATE_ERROR;
+    BSL_ERR_PUSH_ERROR(ret);
+ERR:
+    RefreshCtx(ctx, x, y, ret);
+    BN_Destory(twoPowN);
+    BN_MontDestory(mont);
+    BN_OptimizerDestory(opt);
+    return ret;
+}
+
+static int32_t DH_GenSp80056ASafePrime(CRYPT_DH_Ctx *ctx)
+{
     int32_t ret;
-    int32_t cnt;
+    uint32_t bits = BN_Bits(ctx->para->p);
     BN_BigNum *x = BN_Create(BN_Bits(ctx->para->p) + 1);
     BN_BigNum *y = BN_Create(BN_Bits(ctx->para->p));
     BN_BigNum *minP = BN_Create(BN_Bits(ctx->para->p) + 1);
@@ -571,7 +595,7 @@ int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx)
         BSL_ERR_PUSH_ERROR(ret);
         goto EXIT;
     }
-    for (cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
+    for (int32_t cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
         /*  Generate private key x for [1, q-1] or [1, p-2] */
         ret = BN_RandRangeEx(ctx->libCtx, x, xLimb);
         if (ret != CRYPT_SUCCESS) {
@@ -605,6 +629,18 @@ EXIT:
     BN_MontDestroy(mont);
     BN_OptimizerDestroy(opt);
     return ret;
+}
+
+int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx) {
+    if (ctx == NULL || ctx->para == NULL) {
+        BSL_ERR_PUSH_ERROR(CRYPT_DH_PARA_ERROR);
+        return CRYPT_DH_PARA_ERROR;
+    }
+    int32_t s = CRYPT_DH_GetSecBits(ctx);
+    if (ctx->para->q != NULL && s != 0) {
+        return DH_GenSp80056ATestCandidates(ctx);
+    }
+    return DH_GenSp80056ASafePrime(ctx);
 }
 
 static int32_t ComputeShareKeyInputCheck(const CRYPT_DH_Ctx *ctx, const CRYPT_DH_Ctx *pubKey,
@@ -668,7 +704,7 @@ int32_t CRYPT_DH_ComputeShareKey(const CRYPT_DH_Ctx *ctx, const CRYPT_DH_Ctx *pu
         goto EXIT;
     }
     /* Check whether the public key meets the requirements. */
-    ret = PubCheck(pubKey->y, minP, ctx->para->q, mont, opt);
+    ret = PubCheck(pubKey->y, minP);
     if (ret != CRYPT_SUCCESS) {
         BSL_ERR_PUSH_ERROR(ret);
         goto EXIT;

openharmony/third_party_openhitls

Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/245

crypto/dh/src/dh_core.c
@@ -471,14 +471,7 @@ int32_t CRYPT_DH_GetPara(const CRYPT_DH_Ctx *ctx, BSL_Param *param)
     return GetDhParamG(ctx->para, param);
 }
 
-/**
-    y != 0
-    y != 1
-    y < p - 1
-    (y ^ q) mod p == 1
-*/
-static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP,
-    const BN_BigNum *q, BN_Mont *montP, BN_Optimizer *opt)
+static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP)
 {
     // y != 0, y != 1
     if (BN_IsZero(y) || BN_IsOne(y)) {
@@ -490,27 +483,6 @@ static int32_t PubCheck(const BN_BigNum *y, const BN_BigNum *minP,
         BSL_ERR_PUSH_ERROR(CRYPT_DH_KEYINFO_ERROR);
         return CRYPT_DH_KEYINFO_ERROR;
     }
-    if (q == NULL) {
-        return CRYPT_SUCCESS; // The parameter q does not exist, this function is ended early.
-    }
-    // Verify q.
-    BN_BigNum *r = BN_Create(BN_Bits(minP));
-    if (r == NULL) {
-        BSL_ERR_PUSH_ERROR(CRYPT_MEM_ALLOC_FAIL);
-        return CRYPT_MEM_ALLOC_FAIL;
-    }
-    int32_t ret = BN_MontExp(r, y, q, montP, opt);
-    if (ret != CRYPT_SUCCESS) {
-        BN_Destroy(r);
-        BSL_ERR_PUSH_ERROR(ret);
-        return ret;
-    }
-    if (!BN_IsOne(r)) {
-        BN_Destroy(r);
-        BSL_ERR_PUSH_ERROR(CRYPT_DH_KEYINFO_ERROR);
-        return CRYPT_DH_KEYINFO_ERROR;
-    }
-    BN_Destroy(r);
     return CRYPT_SUCCESS;
 }
 
@@ -538,22 +510,86 @@ static void RefreshCtx(CRYPT_DH_Ctx *dhCtx, BN_BigNum *x, BN_BigNum *y, int32_t 
     }
 }
 
-int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx)
+/* SP800-56Ar3 5.6.1.1.4 Key-Pair Generation by Testing Candidates */
+static int32_t DH_GenSp80056ATestCandidates(CRYPT_DH_Ctx *ctx)
 {
-    if (ctx == NULL) {
-        BSL_ERR_PUSH_ERROR(CRYPT_NULL_INPUT);
-        return CRYPT_NULL_INPUT;
-    }
-    if (ctx->para == NULL) {
+    int32_t ret;
+    uint32_t bits = BN_Bits(ctx->para->p);
+    uint32_t qbits = BN_Bits(ctx->para->q);
+    /* If s is not the maximum security strength that can be support by (p, q, g), then return an error. */
+    uint32_t s = (uint32_t)CRYPT_DH_GetSecBits(ctx);
+    if (bits == 0 || qbits == 0 || s == 0) {
         BSL_ERR_PUSH_ERROR(CRYPT_DH_PARA_ERROR);
         return CRYPT_DH_PARA_ERROR;
     }
+    /* 2*s <= n <= len(q), set n = 2*s */
+    uint32_t n = 2 * s;
+    BN_BigNum *x = BN_Create(bits);
+    BN_BigNum *y = BN_Create(bits);
+    BN_BigNum *twoPowN = BN_Create(n);
+    BN_Mont *mont = BN_MontCreate(ctx->para->p);
+    BN_BigNum *m = ctx->para->q;
+    BN_Optimizer *opt = BN_OptimizerCreate();
+    if (x == NULL || y == NULL || mont == NULL || opt == NULL || twoPowN == NULL) {
+        ret = CRYPT_MEM_ALLOC_FAIL;
+        BSL_ERR_PUSH_ERROR(CRYPT_MEM_ALLOC_FAIL);
+        goto ERR;
+    }
+    ret = BN_SetLimb(twoPowN, 1);
+    if (ret != CRYPT_SUCCESS) {
+        BSL_ERR_PUSH_ERROR(ret);
+        goto ERR;
+    }
+    ret = BN_Lshift(twoPowN, twoPowN, n);
+    if (ret != CRYPT_SUCCESS) {
+        BSL_ERR_PUSH_ERROR(ret);
+        goto ERR;
+    }
+    /* Set M = min(2^N, q), the minimum of 2^N and q. */
+    if (BN_Cmp(twoPowN, m) < 0) {
+        m = twoPowN;
+    }
+    for (int32_t cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
+        /* c in the interval [0, 2N - 1] */
+        ret = BN_RandRangeEx(ctx->libCtx, x, twoPowN);
+        if (ret != CRYPT_SUCCESS) {
+            BSL_ERR_PUSH_ERROR(ret);
+            goto ERR;
+        }
+        /* x = c + 1 */
+        ret = BN_AddLimb(x, x, 1);
+        if (ret != CRYPT_SUCCESS) {
+            BSL_ERR_PUSH_ERROR(ret);
+            goto ERR;
+        }
+        /* If c > M - 2, (i.e. c + 1 >= M) continue */
+        if (BN_Cmp(x, m) >= 0) {
+            continue;
+        }
+        ret = BN_MontExpConsttime(y, ctx->para->g, x, mont, opt);
+        if (ret != CRYPT_SUCCESS) {
+            BSL_ERR_PUSH_ERROR(ret);
+        }
+        goto ERR; // The function exits successfully.
+    }
+    ret = CRYPT_DH_RAND_GENERATE_ERROR;
+    BSL_ERR_PUSH_ERROR(ret);
+ERR:
+    RefreshCtx(ctx, x, y, ret);
+    BN_Destroy(twoPowN);
+    BN_MontDestroy(mont);
+    BN_OptimizerDestroy(opt);
+    return ret;
+}
+
+static int32_t DH_GenSp80056ASafePrime(CRYPT_DH_Ctx *ctx)
+{
     int32_t ret;
-    int32_t cnt;
-    BN_BigNum *x = BN_Create(BN_Bits(ctx->para->p) + 1);
-    BN_BigNum *y = BN_Create(BN_Bits(ctx->para->p));
-    BN_BigNum *minP = BN_Create(BN_Bits(ctx->para->p) + 1);
-    BN_BigNum *xLimb = BN_Create(BN_Bits(ctx->para->p) + 1);
+    uint32_t bits = BN_Bits(ctx->para->p);
+    BN_BigNum *x = BN_Create(bits);
+    BN_BigNum *y = BN_Create(bits);
+    BN_BigNum *minP = BN_Create(bits);
+    BN_BigNum *xLimb = BN_Create(bits);
     BN_Mont *mont = BN_MontCreate(ctx->para->p);
     BN_Optimizer *opt = BN_OptimizerCreate();
     if (x == NULL || y == NULL || minP == NULL || xLimb == NULL || mont == NULL || opt == NULL) {
@@ -571,7 +607,7 @@ int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx)
         BSL_ERR_PUSH_ERROR(ret);
         goto EXIT;
     }
-    for (cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
+    for (int32_t cnt = 0; cnt < CRYPT_DH_TRY_CNT_MAX; cnt++) {
         /*  Generate private key x for [1, q-1] or [1, p-2] */
         ret = BN_RandRangeEx(ctx->libCtx, x, xLimb);
         if (ret != CRYPT_SUCCESS) {
@@ -605,6 +641,19 @@ EXIT:
     BN_MontDestroy(mont);
     BN_OptimizerDestroy(opt);
     return ret;
+}
+
+int32_t CRYPT_DH_Gen(CRYPT_DH_Ctx *ctx)
+{
+    if (ctx == NULL || ctx->para == NULL) {
+        BSL_ERR_PUSH_ERROR(CRYPT_DH_PARA_ERROR);
+        return CRYPT_DH_PARA_ERROR;
+    }
+    int32_t s = CRYPT_DH_GetSecBits(ctx);
+    if (ctx->para->q != NULL && s != 0) {
+        return DH_GenSp80056ATestCandidates(ctx);
+    }
+    return DH_GenSp80056ASafePrime(ctx);
 }
 
 static int32_t ComputeShareKeyInputCheck(const CRYPT_DH_Ctx *ctx, const CRYPT_DH_Ctx *pubKey,
@@ -668,7 +717,7 @@ int32_t CRYPT_DH_ComputeShareKey(const CRYPT_DH_Ctx *ctx, const CRYPT_DH_Ctx *pu
         goto EXIT;
     }
     /* Check whether the public key meets the requirements. */
-    ret = PubCheck(pubKey->y, minP, ctx->para->q, mont, opt);
+    ret = PubCheck(pubKey->y, minP);
     if (ret != CRYPT_SUCCESS) {
         BSL_ERR_PUSH_ERROR(ret);
         goto EXIT;
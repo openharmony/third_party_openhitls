de3ef7e397938f2d2fb9014bb58a95226baeb6fb
dny <duanningyan@huawei.com>
2025-07-22 20:07:38 +0800

    delete cmvp feature and change CRYPT_CMVP_Selftest param.
    Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/396
    
    Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

--- crypto/provider/src/cmvp/cmvp_utils/cmvp_selftest_ecdh.c
+++ crypto/provider/src/cmvp/cmvp_utils/cmvp_selftest_ecdh.c
@@ -14,7 +14,7 @@
  */
 
 #include "hitls_build.h"
-#ifdef HITLS_CRYPTO_CMVP
+#if defined(HITLS_CRYPTO_CMVP_ISO19790) || defined(HITLS_CRYPTO_CMVP_GM) || defined(HITLS_CRYPTO_CMVP_FIPS)
 
 #include <string.h>
 #include "crypt_cmvp_selftest.h"
@@ -164,4 +164,4 @@ bool CRYPT_CMVP_SelftestProviderEcdh(void *libCtx, const char *attrName)
     return CRYPT_CMVP_SelftestEcdhInternal(libCtx, attrName);
 }
 
-#endif // HITLS_CRYPTO_CMVP
+#endif /* HITLS_CRYPTO_CMVP_ISO19790 || HITLS_CRYPTO_CMVP_GM || HITLS_CRYPTO_CMVP_FIPS */

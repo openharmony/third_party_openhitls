xihuanyexiu/openHiTLS

Modify the ecc addition check
Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/462

testcode/sdv/testcase/crypto/ecc/test_suite_sdv_eal_ecdh.c
@@ -892,3 +892,64 @@ EXIT:
     TestRandDeInit();
 }
 /* END_CASE */
+
+
+/**
+ * @test   SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001
+ * @title  ECDH SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001 test.
+ */
+/* BEGIN_CASE */
+void SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001(int paraId, Hex *bk1, Hex *bk2, Hex *bk3)
+{
+    if (IsCurveDisabled(paraId) && paraId != CRYPT_ECC_SM2) {
+        SKIP_TEST();
+    }
+    TestMemInit();
+    ASSERT_EQ(TestRandInit(), CRYPT_SUCCESS);
+    ECC_Para *para = ECC_NewPara(paraId);
+    ECC_Point *twoG = ECC_NewPoint(para);
+    ECC_Point *pt1 = ECC_NewPoint(para);
+    ECC_Point *pt2 = ECC_NewPoint(para);
+    ECC_Point *r1 = ECC_NewPoint(para);
+    ECC_Point *r2 = ECC_NewPoint(para);
+
+    BN_BigNum *bnk1 = BN_Create(0);
+    BN_BigNum *bnk2 = BN_Create(0);
+    BN_BigNum *bnk3 = BN_Create(0);
+
+    ASSERT_TRUE(bnk1 != NULL);
+    ASSERT_TRUE(bnk2 != NULL);
+    ASSERT_TRUE(bnk3 != NULL);
+    ASSERT_TRUE(twoG != NULL);
+    ASSERT_TRUE(pt1 != NULL);
+    ASSERT_TRUE(pt2 != NULL);
+    ASSERT_TRUE(r1 != NULL);
+    ASSERT_TRUE(r2 != NULL);
+
+    ASSERT_EQ(BN_Bin2Bn(bnk1, bk1->x, bk1->len), CRYPT_SUCCESS);
+    ASSERT_EQ(BN_Bin2Bn(bnk2, bk2->x, bk2->len), CRYPT_SUCCESS);
+    ASSERT_EQ(BN_Bin2Bn(bnk3, bk3->x, bk3->len), CRYPT_SUCCESS);
+    // cal 2*G
+    ASSERT_EQ(ECP_PointMulFast(para, twoG, bnk3, NULL), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECP_PointMulFast(para, pt1, bnk1, NULL), CRYPT_SUCCESS);
+    ASSERT_EQ(ECP_PointMulFast(para, pt2, bnk2, twoG), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECC_PointAddAffine(para, r1, pt1, pt2), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECC_PointMulAdd(para, r2, bnk1, bnk2, twoG), CRYPT_SUCCESS);
+    ASSERT_EQ(ECC_PointCmp(para, r1, r2), CRYPT_SUCCESS);
+
+EXIT:
+    ECC_FreePara(para);
+    ECC_FreePoint(twoG);
+    ECC_FreePoint(pt1);
+    ECC_FreePoint(pt2);
+    ECC_FreePoint(r1);
+    ECC_FreePoint(r2);
+    BN_Destroy(bnk1);
+    BN_Destroy(bnk2);
+    BN_Destroy(bnk3);
+    TestRandDeInit();
+}
+/* END_CASE */

openharmony/third_party_openhitls

Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/465
Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

testcode/sdv/testcase/crypto/ecc/test_suite_sdv_eal_ecdh.c
@@ -15,6 +15,7 @@
 /* INCLUDE_BASE test_suite_sdv_eal_ecc */
 
 /* BEGIN_HEADER */
+#include "ecc_local.h"
 /* END_HEADER */
 #define ECDH_MAX_BIT_LEN 521
 
@@ -716,5 +717,65 @@ void SDV_CRYPTO_ECDH_GET_SEC_BITS_FUNC_TC001(int paraid, int secBits)
     ASSERT_EQ(CRYPT_EAL_PkeyGetSecurityBits(pkey), secBits);
 EXIT:
     CRYPT_EAL_PkeyFreeCtx(pkey);
+}
+/* END_CASE */
+
+/**
+ * @test   SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001
+ * @title  ECDH SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001 test.
+ */
+/* BEGIN_CASE */
+void SDV_CRYPTO_ECC_ADD_CAL_TEST_FUNC_TC001(int paraId, Hex *bk1, Hex *bk2, Hex *bk3)
+{
+    if (IsCurveDisabled(paraId) && paraId != CRYPT_ECC_SM2) {
+        SKIP_TEST();
+    }
+    TestMemInit();
+    ASSERT_EQ(TestRandInit(), CRYPT_SUCCESS);
+    ECC_Para *para = ECC_NewPara(paraId);
+    ECC_Point *twoG = ECC_NewPoint(para);
+    ECC_Point *pt1 = ECC_NewPoint(para);
+    ECC_Point *pt2 = ECC_NewPoint(para);
+    ECC_Point *r1 = ECC_NewPoint(para);
+    ECC_Point *r2 = ECC_NewPoint(para);
+
+    BN_BigNum *bnk1 = BN_Create(0);
+    BN_BigNum *bnk2 = BN_Create(0);
+    BN_BigNum *bnk3 = BN_Create(0);
+
+    ASSERT_TRUE(bnk1 != NULL);
+    ASSERT_TRUE(bnk2 != NULL);
+    ASSERT_TRUE(bnk3 != NULL);
+    ASSERT_TRUE(twoG != NULL);
+    ASSERT_TRUE(pt1 != NULL);
+    ASSERT_TRUE(pt2 != NULL);
+    ASSERT_TRUE(r1 != NULL);
+    ASSERT_TRUE(r2 != NULL);
+
+    ASSERT_EQ(BN_Bin2Bn(bnk1, bk1->x, bk1->len), CRYPT_SUCCESS);
+    ASSERT_EQ(BN_Bin2Bn(bnk2, bk2->x, bk2->len), CRYPT_SUCCESS);
+    ASSERT_EQ(BN_Bin2Bn(bnk3, bk3->x, bk3->len), CRYPT_SUCCESS);
+    // cal 2*G
+    ASSERT_EQ(ECC_PointMul(para, twoG, bnk3, NULL), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECC_PointMul(para, pt1, bnk1, NULL), CRYPT_SUCCESS);
+    ASSERT_EQ(ECC_PointMul(para, pt2, bnk2, twoG), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECC_PointAddAffine(para, r1, pt1, pt2), CRYPT_SUCCESS);
+
+    ASSERT_EQ(ECC_PointMulAdd(para, r2, bnk1, bnk2, twoG), CRYPT_SUCCESS);
+    ASSERT_EQ(ECC_PointCmp(para, r1, r2), CRYPT_SUCCESS);
+
+EXIT:
+    ECC_FreePara(para);
+    ECC_FreePoint(twoG);
+    ECC_FreePoint(pt1);
+    ECC_FreePoint(pt2);
+    ECC_FreePoint(r1);
+    ECC_FreePoint(r2);
+    BN_Destroy(bnk1);
+    BN_Destroy(bnk2);
+    BN_Destroy(bnk3);
+    TestRandDeInit();
 }
 /* END_CASE */
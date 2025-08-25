2787750e772c175e009ffaa6f9902f3da609130a
Dongjianwei001 <dongjianwei1@huawei.com>
2025-08-25 15:12:12 +0800

    Fix code compilation warnings
    Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/491
    
    Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

--- tls/feature/security/src/security_default.c
+++ tls/feature/security/src/security_default.c
@@ -147,7 +147,30 @@ int32_t SECURITY_DefaultCb(const HITLS_Ctx *ctx, const HITLS_Config *config, int
         case HITLS_SECURITY_SECOP_CIPHER_SHARED:
         case HITLS_SECURITY_SECOP_CIPHER_CHECK:
             /* Check the algorithm suite. */
-            return CheckCipherSuite(other, level);
+            ret = CheckCipherSuite(other, level);
+            break;
+        case HITLS_SECURITY_SECOP_SIGALG_SUPPORTED:
+        case HITLS_SECURITY_SECOP_SIGALG_SHARED:
+        case HITLS_SECURITY_SECOP_SIGALG_CHECK:
+            /* Check the signature algorithm. */
+            schemeInfo = ConfigGetSignatureSchemeInfo(config, (uint16_t)id);
+            if (schemeInfo != NULL && schemeInfo->secBits >= g_minBits[level - 1]) {
+                ret = SECURITY_SUCCESS;
+            } else {
+                ret = SECURITY_ERR;
+            }
+            break;
+        case HITLS_SECURITY_SECOP_CURVE_SUPPORTED:
+        case HITLS_SECURITY_SECOP_CURVE_SHARED:
+        case HITLS_SECURITY_SECOP_CURVE_CHECK:
+            /* Check the group. */
+            groupInfo = ConfigGetGroupInfo(config, (uint16_t)id);
+            if (groupInfo != NULL && groupInfo->secBits >= g_minBits[level - 1]) {
+                ret = SECURITY_SUCCESS;
+            } else {
+                ret = SECURITY_ERR;
+            }
+            break;
         case HITLS_SECURITY_SECOP_TICKET:
             /* Check the session ticket. */
             ret = CheckSessionTicket(level);

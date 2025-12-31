b8261230b7ee6a7784c4ba605155b9d85aad5164
jchx <chenqi180@huawei.com>
2025-12-31 16:24:32 +0800

    fix the order of errors and keys in the crypto module.
    Co-authored-by: jchx<chenqi180@huawei.com>
    
    # message auto-generated for no-merge-commit merge:
    !972 merge fix into main
    
    fix the order of errors and keys in the crypto module.
    
    Created-by: jchx
    Commit-by: jchx
    Merged-by: liwei3013
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openHiTLS/openhitls/wiki/FAQ-社区需求贡献流程.md
    
    -->
    
    **What does this PR do / why do we need it**:
    
    **Self-checklist**:（**请自检，在[ ]内打上x**）
    
    + - [ ] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [ ] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [ ] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    See merge request: openHiTLS/openhitls!972
    
    Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

--- include/crypto/crypt_params_key.h
+++ include/crypto/crypt_params_key.h
@@ -157,7 +157,7 @@ extern "C" {
 #define CRYPT_PARAM_MD_BLOCK_SIZE                           (CRYPT_PARAM_MD_BASE + 2)
 #define CRYPT_PARAM_MD_ATTR                                 (CRYPT_PARAM_MD_BASE + 3)
 
-#define CRYPT_PARAM_XMSS_BASE                               1900
+#define CRYPT_PARAM_XMSS_BASE                               1950
 #define CRYPT_PARAM_XMSS_PRV_INDEX                          (CRYPT_PARAM_XMSS_BASE + 1)
 #define CRYPT_PARAM_XMSS_PRV_SEED                           (CRYPT_PARAM_XMSS_BASE + 2)
 #define CRYPT_PARAM_XMSS_PRV_PRF                            (CRYPT_PARAM_XMSS_BASE + 3)
@@ -165,11 +165,11 @@ extern "C" {
 #define CRYPT_PARAM_XMSS_PUB_ROOT                           (CRYPT_PARAM_XMSS_BASE + 5)
 #define CRYPT_PARAM_XMSS_XDR_TYPE                           (CRYPT_PARAM_XMSS_BASE + 6)
 
-#define CRYPT_PARAM_FRODOKEM_BASE                           1950
+#define CRYPT_PARAM_FRODOKEM_BASE                           2000
 #define CRYPT_PARAM_FRODOKEM_PRVKEY                         (CRYPT_PARAM_FRODOKEM_BASE + 1)
 #define CRYPT_PARAM_FRODOKEM_PUBKEY                         (CRYPT_PARAM_FRODOKEM_BASE + 2)
 
-#define CRYPT_PARAM_MCELIECE_BASE                           2000
+#define CRYPT_PARAM_MCELIECE_BASE                           2050
 #define CRYPT_PARAM_MCELIECE_PRVKEY                         (CRYPT_PARAM_MCELIECE_BASE + 1)
 #define CRYPT_PARAM_MCELIECE_PUBKEY                         (CRYPT_PARAM_MCELIECE_BASE + 2)
 

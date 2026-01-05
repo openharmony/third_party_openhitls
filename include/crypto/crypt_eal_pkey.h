58cf55a1d367a58c542ae0440f8c5586bb230669
dsy678 <dongshunyu1@huawei.com>
2026-01-05 21:52:14 +0800

    Added RSA9796-2 comments and added Retail MAC algorithm ID.
    Co-authored-by: dsy<dongshunyu1@huawei.com>
    
    # message auto-generated for no-merge-commit merge:
    !977 merge algfix0.2 into openhitls-0.2
    
    Added RSA9796-2 comments and added Retail MAC algorithm ID.
    
    Created-by: dsy678
    Commit-by: dsy
    Merged-by: liwei3013
    Description: Added RSA9796-2 comments and added Retail MAC algorithm ID.
    
    See merge request: openHiTLS/openhitls!977
    
    Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

--- include/crypto/crypt_eal_pkey.h
+++ include/crypto/crypt_eal_pkey.h
@@ -369,7 +369,7 @@ int32_t CRYPT_EAL_PkeyVerify(const CRYPT_EAL_PkeyCtx *pkey, CRYPT_MD_AlgId id, c
 
 /**
  * @ingroup crypt_eal_pkey
- * @brief   Sign hash data
+ * @brief   Sign hash data(RSA9796-2 Signature is not supported)
  *
  * @param   pkey      [IN] Key session
  * @param   hash      [IN] Hash data
@@ -389,7 +389,7 @@ int32_t CRYPT_EAL_PkeySignData(const CRYPT_EAL_PkeyCtx *pkey, const uint8_t *has
 
 /**
  * @ingroup crypt_eal_pkey
- * @brief   Verify the signature of the hash data
+ * @brief   Verify the signature of the hash data(RSA9796-2 Signature is not supported)
  *
  * @param   pkey     [IN] Key session
  * @param   hash     [IN] Hash data

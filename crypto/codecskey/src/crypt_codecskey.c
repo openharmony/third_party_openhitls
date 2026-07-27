42d191b46bf01c1872b076aa1bdc60593cbe379f
liwei3013 <liwei455@huawei.com>
2026-07-27 02:43:29 +0000

    fix(codecskey): preserve input buffer during PEM key decoding
    Use local cursors when decoding PEM public keys to prevent modification
    of the caller-owned BSL_Buffer descriptor.
    
    Add regression coverage for public and private key decoding, including
    RSA PKCS#1, EC SEC1, encrypted and unencrypted PKCS#8, ASN.1, automatic
    format detection, provider decoding, and error paths.
    
    Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/1731
    
    Signed-off-by: Dongjianwei001 <dongjianwei1@huawei.com>

--- crypto/codecskey/src/crypt_codecskey.c
+++ crypto/codecskey/src/crypt_codecskey.c
@@ -216,12 +216,14 @@ int32_t CRYPT_EAL_ParsePemPubKey(CRYPT_EAL_LibCtx *libctx, const char *attrName,
     CRYPT_EAL_PkeyCtx **ealPubKey)
 {
     BSL_PEM_Symbol symbol = {0};
+    uint8_t *buff = encode->data;
+    uint32_t buffLen = encode->dataLen;
     int32_t ret = EAL_GetPemPubKeySymbol(type, &symbol);
     if (ret != CRYPT_SUCCESS) {
         return ret;
     }
     BSL_Buffer asn1 = {0};
-    ret = BSL_PEM_DecodePemToAsn1((char **)&(encode->data), &(encode->dataLen), &symbol, &(asn1.data), &(asn1.dataLen));
+    ret = BSL_PEM_DecodePemToAsn1((char **)&buff, &buffLen, &symbol, &(asn1.data), &(asn1.dataLen));
     if (ret != BSL_SUCCESS) {
         BSL_ERR_PUSH_ERROR(ret);
         return ret;
